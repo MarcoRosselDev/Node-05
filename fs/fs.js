5bafae1359434a0cf7e703acf263219b3354e9af
Marco Rossel <andresmarcorossel@gmail.com>
2023-09-18 18:39:17 -0300

    node | notes reminder course 2

---
+++ fs/fs.js
@@ -0,0 +1,22 @@
+const fs = require('fs')
+//console.log(fs);
+
+fs.readFile('./fs/index.html', 'utf-8', (err, data)=>{
+    if (err) {
+        console.log(err);
+    } else{
+        console.log(data);
+    }
+})
+
+/* <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Title</title>
+</head>
+<body>
+    <h1>Hola mundo</h1>
+</body>
+</html> */
