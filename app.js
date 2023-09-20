1e04a290b1d8779e2e9a210c3d9f5b9734093fad
Marco Rossel <andresmarcorossel@gmail.com>
2023-09-20 16:46:06 -0300

    node | params route

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const cursos = require('./cursos.json')
 
 app.get('/', (req, res) => {
@@ -8,8 +8,27 @@ app.get('/', (req, res) => {
 })
 
 app.get('/cursos', (req, res)=>{
-  res.send(JSON.stringify(cursos))
+  res.status(200).send(JSON.stringify(cursos))
 })
+
+app.get('/cursos/programacion', (req, res) =>{
+  res.status(200).send(JSON.stringify(cursos.programacion))
+})
+
+app.get('/cursos/matematicas', (req, res) =>{
+  res.status(200).send(JSON.stringify(cursos.mates))
+})
+
+app.get('/cursos/programacion:lenguaje', (req, res) =>{
+  const param = res.param.lenguaje;
+  if (true) {
+    const filt = cursos.programacion.filter(e => e.curso == param)
+    res.status(200).send(JSON.stringify(filt))
+  }
+  res.status(404).send(`No se encontraron cursos de ${param}`)
+})
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
