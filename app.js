0c4a9a06f9470438be41b10cc1cf91ac4be52072
Marco Rossel <andresmarcorossel@gmail.com>
2023-09-21 11:58:33 -0300

    node | john course | back res.end

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log(__dirname);
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
+  const url = req.url
+  if (url == "/") {
+    res.statusCode = 200;
+    res.end('Hello World');
+  } else if (url == '/home') {
+    res.statusCode = 200;
+    res.end('home page');
+  }
+  else{
+    res.statusCode = 404;
+    res.end('page not found, flease back to home page');
+  }
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
