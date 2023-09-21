5a635bbadb10b9d32f21334e08bcc0a69c0d3b5e
Marco Rossel <andresmarcorossel@gmail.com>
2023-09-20 21:57:06 -0300

    node | john course | break

--- app.js
+++ app.js
@@ -1,12 +1,1 @@
-const utils = require('./utils');
-
-utils.sayHi('marco')
-
-console.log(utils.add(5, 3));
-console.log(utils.subtract(5, 3));
-
-/*------output--------
-
-Hello there marco
-8
-2 */
+console.log(__dirname);
