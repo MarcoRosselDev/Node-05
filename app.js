679e8df3b235df4c7f9d3efd2d4434c58e4cb5cd
Marco Rossel <andresmarcorossel@gmail.com>
2023-09-22 19:43:50 -0300

    node | john course | vanilla node

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
-const EventEmitter = require('events');
+const http = require('http');
 
-const customEmitter = new EventEmitter()
-
-customEmitter.on('randomName', (age, name)=>{
-  console.log(`name: ${name}, age: ${age}`);
+const server = http.createServer((req, res)=>{
+  console.log('Hello word');// en consola de node
+  const url = req.url
+  console.log(url); // --->"/"
+  const method = req.method
+  console.log(method); // GET
+  res.writeHead(
+    200,          // status code
+    {'contant-type': 'text/html'}// 'text/plane' ---> ojo
+    )
+  res.write(
+    `<h1>Home page</h1>
+    <p>porfavor <a href="http://localhost:3000/home">click</a> para var mas</p>`
+  )
+  res.end()
 })
 
-customEmitter.on('randomName', (a, b)=>{
-  console.log(`data ${a} ${b}`);
-})
-
-customEmitter.emit('randomName', 28, 'marco')
+server.listen(5000)
