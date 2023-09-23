77c83015a3ee3217d3aee3627013ff5b4e945b6e
Marco Rossel <andresmarcorossel@gmail.com>
2023-09-23 12:59:23 -0300

    node | john course | todo list test

--- app.js
+++ app.js
@@ -1,31 +1,23 @@
 const http = require('http');
+const {readFileSync} = require('fs')
+
+const todoListHTML = readFileSync('./todo_list/index.html')
+const todoListCSS = readFileSync('./todo_list/style.css')
 
 const server = http.createServer((req, res)=>{
-  console.log('Hello word');// en consola de node
   const url = req.url
-  console.log(url); // --->"/"
-  const method = req.method
-  console.log(method); // GET
-  res.writeHead(
-    200,          // status code
-    {'content-type': 'text/html'
-  }// 'text/plane' ---> ojo
-    )
-  res.write(
-    `<h1>Home page</h1>
-    <p>porfavor <a href="http://localhost:3000/home">click</a> para var mas</p>`
-  )/* 
-  res.writeHead(200, {'content-type': 'text/css'})
-  res.write(`
-  *{
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-    background-color: black;
-    color: aliceblue;
+  console.log(url);
+
+  if (url === '/') {
+    res.writeHead(200,{'content-type': 'text/html'})
+    res.write(todoListHTML)
+    res.end()
+  } else if (url === '/style.css') {
+    res.writeHead(200,{'content-type': 'text/css'})
+    res.write(todoListCSS)
+    res.end()
   }
-  `) */
-  res.end()
+
 })
 
 server.listen(5000)
