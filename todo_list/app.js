42e8cc8f564dddb2fc5c880d39889894c3b82bc4
Marco Rossel <andresmarcorossel@gmail.com>
2023-09-23 17:40:41 -0300

    node | john course | vanilla node 5

--- todo_list/app.js
+++ todo_list/app.js
@@ -2,7 +2,14 @@ const input = document.querySelector('input');
 const button = document.querySelector('button');
 const tasks = document.querySelector('.tasks');
 
+let lista = []
+
 button.addEventListener('click', function () {
-    console.log(input.value);
-    tasks.innerHTML = `<p>${input.value}</p>`;
+    lista.push(input.value)
+    let printDom = ''
+    for (let i = 0; i < lista.length; i++) {
+        printDom += `<li>${lista[i]}</li>`;
+    }
+    tasks.children[0].innerHTML = printDom;
+    input.value = '';
 });
