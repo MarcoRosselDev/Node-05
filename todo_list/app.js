246cc8b871f4a94a657a3c8b5a09792f6404f141
Marco Rossel <andresmarcorossel@gmail.com>
2023-09-23 14:56:37 -0300

    node | john course | vanilla node 4

--- todo_list/app.js
+++ todo_list/app.js
@@ -1,1 +1,8 @@
-console.log('Hola mundo!');
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+const tasks = document.querySelector('.tasks');
+
+button.addEventListener('click', function () {
+    console.log(input.value);
+    tasks.innerHTML = `<p>${input.value}</p>`;
+});
