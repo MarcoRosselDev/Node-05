32432d1c1b7f59389b9f55baee9eca0ee74a0145
Marco Rossel <andresmarcorossel@gmail.com>
2023-09-22 21:35:46 -0300

    node | john course | vanilla node 2

--- app.js
+++ app.js
@@ -8,12 +8,23 @@ const server = http.createServer((req, res)=>{
   console.log(method); // GET
   res.writeHead(
     200,          // status code
-    {'contant-type': 'text/html'}// 'text/plane' ---> ojo
+    {'content-type': 'text/html'
+  }// 'text/plane' ---> ojo
     )
   res.write(
     `<h1>Home page</h1>
     <p>porfavor <a href="http://localhost:3000/home">click</a> para var mas</p>`
-  )
+  )/* 
+  res.writeHead(200, {'content-type': 'text/css'})
+  res.write(`
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    background-color: black;
+    color: aliceblue;
+  }
+  `) */
   res.end()
 })
 
