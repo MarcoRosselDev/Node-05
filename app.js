8435d408410b7debe9b8aa5aa2ae7e9f1aebf399
Marco Rossel <andresmarcorossel@gmail.com>
2023-09-24 12:28:29 -0300

    node | john course | express | send one id user

--- app.js
+++ app.js
@@ -6,20 +6,30 @@ const {products} = require('./data')
 
 app.use(express.static('./public')) // realmente poderozo, por que con node tendriamos que realizar 3000 respuestas si tubiesemos 3000 ptericiones de imagenes y recursos
 
+// get url '/'
 app.get('/', (req, res) => {
   console.log(req.url);// -->'/'
-  //res.writeHead(200, 'successful', {'content-type': 'text/html'})
-  res.status(200).sendFile(path.resolve(__dirname, './todo_list/index.html'))
+  //res.writeHead(200, 'successful', {'content-type': 'text/html'})--> codigo de node vanilla
+  res.status(200).sendFile(path.resolve(__dirname, './todo_list/index.html')) // --> express
 })
 
-app.get('/json', (req, res)=>{
+// send a json object
+app.get('/users', (req, res)=>{
   res.json(products)// api res--->json data send
 })
 
+// get un item de una json object
+app.get('/users/:userID', (req, res)=>{
+  console.log(req.params);
+})
+
+
+// para todos los demas url, send 404, y msj not found
 app.all('*', (req, res) => {
   res.status(404).send('<h1>no se encontro la paguina<h1/>')
 })
 
+// puerto de escucha
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
