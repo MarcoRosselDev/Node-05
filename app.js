8e60887444c9c2056e6de98d9966f66d4d37de02
Marco Rossel <andresmarcorossel@gmail.com>
2023-09-24 00:38:22 -0300

    node | john course | express 3

--- app.js
+++ app.js
@@ -1,40 +1,29 @@
 const express = require('express')
 const app = express()
 const port = 5000
-const fs = require('fs')
-const style = fs.readFileSync('./todo_list/style.css')
+const path = require('path')
+
+app.use(express.static('./public'))
 
 app.get('/', (req, res) => {
   console.log(req.url);// -->'/'
   //res.writeHead(200, 'successful', {'content-type': 'text/html'})
-  res.send(`
-      <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="style.css">
-        <title>To do list</title>
-    </head>
-    <body>
-        <div class="div-list">
-            <h1>TO DO LIST</h1>
-            <p>write some tastk to do</p>
-            <div class="input">
-                <input type="text" placeholder="Write some task">
-                <button>send</button>
-            </div>
-            <div class="tasks">
-                <ul></ul>
-            </div>
-            <a href="http://"></a>
-        </div>
-        <script src="./app.js"></script>
-    </body>
-    </html>
-  `)
+  res.status(200).sendFile(path.resolve(__dirname, './todo_list/index.html'))
+})
+
+app.all('*', (req, res) => {
+  res.status(404).send('<h1>no se encontro la paguina<h1/>')
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+// app.metodos mas comunes
+// app.get
+// app.post
+// app.put
+// app.delete
+// app.all
+// app.use
+// app.listen
