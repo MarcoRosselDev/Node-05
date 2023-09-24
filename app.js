2b7f45750e269920681ff0370fe0f20361e6f655
Marco Rossel <andresmarcorossel@gmail.com>
2023-09-24 03:53:12 -0300

    node | john course | express | send json data

--- app.js
+++ app.js
@@ -2,13 +2,18 @@ const express = require('express')
 const app = express()
 const port = 5000
 const path = require('path')
+const {products} = require('./data')
 
-app.use(express.static('./public'))
+app.use(express.static('./public')) // realmente poderozo, por que con node tendriamos que realizar 3000 respuestas si tubiesemos 3000 ptericiones de imagenes y recursos
 
 app.get('/', (req, res) => {
   console.log(req.url);// -->'/'
   //res.writeHead(200, 'successful', {'content-type': 'text/html'})
   res.status(200).sendFile(path.resolve(__dirname, './todo_list/index.html'))
+})
+
+app.get('/json', (req, res)=>{
+  res.json(products)// api res--->json data send
 })
 
 app.all('*', (req, res) => {
---
+++ data.js
@@ -0,0 +1,24 @@
+products = [{
+    "nombre": "marco",
+    "apellido": "rossel"
+},
+{
+    "nombre": "loco",
+    "apellido": "pepe"
+}]
+
+jsonData = {
+    a :{
+        "date": 1,
+        "bool": false
+    },
+    b :{
+        "date": 1,
+        "bool": false
+    }
+}
+
+module.exports = {
+    products: products,
+    jsonData: jsonData
+}
