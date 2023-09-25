85b652bdd33c4d34384b7a9d2d2d8bb6f372d729
Marco Rossel <andresmarcorossel@gmail.com>
2023-09-24 23:14:12 -0300

    node | john course | express | middleware 2

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res)=>{
+const loger = (req, res, next) =>{
+  const url = req.url;
+  const method = req.method;
+  console.log(url, method, `<--- from ${url} url'`);
+  next(); // <-- clave, si no lo aplico no se puede seguir enviando recursos
+  // middleware, super poderoso para cualquier algorithmo entre peticiones
+  // y para no repetir el codigo
+}
+
+app.get('/', loger, (req, res)=>{
   res.status(200).send(`<p>home page</p>`)
+})
+app.get('/users', loger, (req, res)=>{
+  res.status(200).send(`<p>some users back</p>`)
 })
 
 app.listen(5000, ()=>{
