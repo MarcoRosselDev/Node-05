aa8efc616a25cd8deab2808eedb23ee9f0435102
Marco Rossel <andresmarcorossel@gmail.com>
2023-09-26 19:47:34 -0300

    node | john course | express | controllers

---
+++ controllers/test.js
@@ -0,0 +1,46 @@
+const {people} = require('../data.js');
+
+const getHome = (req, res)=>{
+  res.status(200).json({
+    "success": true,
+    "data": people})
+}
+
+const getHomeID = (req, res)=>{
+  const {id} = req.params;
+  if (id) {
+    const found = people.find(person => person.id === Number(id))
+    if (found) {
+      return res.status(200).json({"data": found})
+    } else {
+      return res.status(404).json({"data": `No se encontro un usuario con el id:${id}` })
+    }
+  }
+  res.status(200).json({
+    "success": true,
+    "data": people})
+}
+
+const postHome = (req, res)=>{
+  const {nombre} = req.body;
+  if (nombre) {
+    const newPeople = [...people];
+    let id = newPeople.length + 1;
+    newPeople.push({
+      "id": id,
+      "name": nombre
+    })
+    return res.status(201).json({
+      "success": true,
+      "data": newPeople
+    })
+  } else {
+    res.status(404).send('por favor incluye un nombre en el cuerpo de la peticion.')
+  }
+}
+
+module.exports = {
+  getHome,
+  getHomeID,
+  postHome
+}
--- routes/home.js
+++ routes/home.js
@@ -1,45 +1,9 @@
 const express = require('express');
-const {people} = require('../data.js')
-
 const router = express.Router();
+const {getHome, getHomeID, postHome} = require('../controllers/test.js')
 
-router.get('/', (req, res)=>{
-    res.status(200).json({
-      "success": true,
-      "data": people})
-  })
-  
-router.get('/:id', (req, res)=>{
-  const {id} = req.params;
-  if (id) {
-    const found = people.find(person => person.id === Number(id))
-    if (found) {
-      return res.status(200).json({"data": found})
-    } else {
-      return res.status(404).json({"data": `No se encontro un usuario con el id:${id}` })
-    }
-  }
-  res.status(200).json({
-    "success": true,
-    "data": people})
-})
-  
-router.post('/', (req, res)=>{
-  const {nombre} = req.body;
-  if (nombre) {
-    const newPeople = [...people];
-    let id = newPeople.length + 1;
-    newPeople.push({
-      "id": id,
-      "name": nombre
-    })
-    return res.status(201).json({
-      "success": true,
-      "data": newPeople
-    })
-  } else {
-    res.status(404).send('por favor incluye un nombre en el cuerpo de la peticion.')
-  }
-})
+router.get('/', getHome)
+router.get('/:id', getHomeID)
+router.post('/', postHome)
 
 module.exports.person = router;
