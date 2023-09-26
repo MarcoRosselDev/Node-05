c9945ac84c70b659b1375eed88ff345f490785e9
Marco Rossel <andresmarcorossel@gmail.com>
2023-09-26 01:32:22 -0300

    node | john course | express | postman test

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const {people} = require('./data.js')
 
 app.use(express.static('./public'));// nombrar index.html al archivo principal o no leera ningun archivo
 app.use(express.urlencoded({extended: false}));// nesecario para codificar el body de el request
@@ -15,6 +16,20 @@ app.post('/login', (req, res)=>{
     return res.status(200).send(`welcome ${user}.`);
   }
   res.status(404).send('por favor ingresa un usuario')
+})
+
+app.get('/api/people', (req, res)=>{
+  res.status(200).json(people)
+})
+
+app.post('/api/people', (req, res) =>{
+  const {name} = req.body;
+  console.log(req.body);
+  console.log(name);
+  if (!name) {
+    return res.status(404).json({"success": false, "response": "Por favor ingrese un nombre."})
+  }
+  res.status(201).json({"success": true, "response": "Usuario creado."})
 })
 
 app.listen(5000, ()=>{
