cb6825a4eb3cc47e839dc27ceab91ebbd3b58182
Marco Rossel <andresmarcorossel@gmail.com>
2023-09-25 22:50:43 -0300

    node | john course | express | middleware 8

--- app.js
+++ app.js
@@ -2,9 +2,16 @@ const express = require('express');
 const app = express();
 
 app.use(express.static('./public'));// nombrar index.html al archivo principal o no leera ningun archivo
+app.use(express.urlencoded({extended: false}));// nesecario para codificar el body de el request
+// ahora podemos ver req.body---> {clave:valor}<---from Payload From Data
 
 app.get('/', (req, res)=>{
   res.status(200).send(login);
+})
+
+app.post('/login', (req, res)=>{
+  console.log(req.body);
+  res.status(200).send('LOGIN SUCCESSFUL')
 })
 
 app.listen(5000, ()=>{
