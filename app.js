d802fbc44478314c9623a84a0755e5510efad758
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-09-27 00:43:23 -0300

    todo list | 1

--- app.js
+++ app.js
@@ -1,18 +1,11 @@
 const express = require("express");
-const { people, products } = require("./data.js");
-const { person } = require("./routes/home.js");
 
 const app = express();
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-app.use("/api", person);
+
+app.get("/api", (req, res) => {
+  res.status(200).send("Hola mundirijillo!");
+});
 
 app.listen(5000, () => {
   console.log("Escuchando en el puerto 5000");
 });
-
-console.log("test from ubuntu");
-console.log("test from ubuntu");
-console.log("test from ubuntu");
-console.log("test from ubuntu");
-console.log("test from ubuntu");
