2b6b5375b5e2b5b1c1f92da8fca5fb85756bad29
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-09-27 23:48:59 -0300

    mongo db | 1

--- app.js
+++ app.js
@@ -4,6 +4,31 @@ const app = express();
 //app.use(express.urlencoded({ extended: false }));
 //app.use(express.json());
 
+require("dotenv").config();
+const password = process.env.PASSWORD;
+const username = process.env.USERNAME_DB;
+const cluster = process.env.CLUSTER;
+
+const url = `mongodb+srv://${username}:${password}@${cluster}.vis1qwi.mongodb.net/?retryWrites=true&w=majority`;
+
+const mongoose = require("mongoose");
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  const a = await mongoose.connect(
+    `mongodb://${username}:${password}@127.0.0.1:27017/test`
+  );
+  if (a) {
+    console.log("success");
+  } else {
+    console.log("filed");
+  }
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+
+main();
+
 app.use(express.static("./public"));
 app.use("/api/v1/tasks", tasks);
 
--- db/connect.js
+++ db/connect.js
@@ -4,3 +4,15 @@ const username = process.env.USERNAME_DB;
 const cluster = process.env.CLUSTER;
 
 const url = `mongodb+srv://${username}:${password}@${cluster}.vis1qwi.mongodb.net/?retryWrites=true&w=majority`;
+
+const mongoose = require("mongoose");
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect(
+    `mongodb://${username}:${password}@127.0.0.1:27017/test`
+  );
+
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
