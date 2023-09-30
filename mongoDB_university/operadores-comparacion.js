bd279d76fa145118b284e2f580f9f03f397a64ff
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-09-30 03:08:40 -0300

    mongo db | mongodb university

---
+++ mongoDB_university/operadores-comparacion.js
@@ -0,0 +1,14 @@
+// posts
+//db.<colecction>.insertOne({...})--->post un documento
+//db.<colecction>.insertMany([{...},{...},{...}])--->post muchos documento
+
+// queris
+//db.<colecction>.find({field:{$eq:<value>}})--->retorna todas las coisidencias del value en el field proporcionado
+//db.<colecction>.find({field:{$in:[<value>,<value>]}})
+// --> retorna los valores que coinsidan en el field proporcionado.
+
+// OPERADORES DE COMPARACION
+//$gt: --->greater than = mayor que = a>b
+//$lt: --->less than = menor que = a<b
+//$lte:--->less than or equal to = a<=b
+//$gte:--->greater than or equal to = a>=b
