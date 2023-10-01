b5c90738231613947e205534d5df67f23b58c017
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-09-30 21:20:11 -0300

    mongo db | mongodb university 4

--- mongoDB_university/operadores-logicos.js
+++ mongoDB_university/operadores-logicos.js
@@ -41,3 +41,134 @@ db.routes.find({
     { $or: [{ "airline.name": "American Airlines" }, { airplane: 320 }] },
   ]
 })
+
+db.sales.find({"Purchase": "online"})
+
+asdfa = 
+  {
+    _id: ObjectId("5bd761dcae323e45a93cd00e"),
+    saleDate: ISODate("2013-09-30T15:18:53.756Z"),
+    items: [
+      {
+        name: 'notepad',
+        tags: [ 'office', 'writing', 'school' ],
+        price: Decimal128("39.91"),
+        quantity: 2
+      },
+      {
+        name: 'notepad',
+        tags: [ 'office', 'writing', 'school' ],
+        price: Decimal128("27.87"),
+        quantity: 3
+      },
+      {
+        name: 'binder',
+        tags: [ 'school', 'general', 'organization' ],
+        price: Decimal128("13.54"),
+        quantity: 4
+      },
+      {
+        name: 'envelopes',
+        tags: [ 'stationary', 'office', 'general' ],
+        price: Decimal128("5.43"),
+        quantity: 9
+      },
+      {
+        name: 'printer paper',
+        tags: [ 'office', 'stationary' ],
+        price: Decimal128("16.15"),
+        quantity: 9
+      },
+      {
+        name: 'pens',
+        tags: [ 'writing', 'office', 'school', 'stationary' ],
+        price: Decimal128("40.01"),
+        quantity: 5
+      },
+      {
+        name: 'envelopes',
+        tags: [ 'stationary', 'office', 'general' ],
+        price: Decimal128("13.37"),
+        quantity: 6
+      },
+      {
+        name: 'laptop',
+        tags: [ 'electronics', 'school', 'office' ],
+        price: Decimal128("1597.31"),
+        quantity: 4
+      },
+      {
+        name: 'notepad',
+        tags: [ 'office', 'writing', 'school' ],
+        price: Decimal128("30.99"),
+        quantity: 4
+      }
+    ],
+    storeLocation: 'Denver',
+    customer: { gender: 'F', age: 59, email: 'veb@molog.eu', satisfaction: 3 },
+    couponUsed: false,
+    purchaseMethod: 'Online'
+  }
+
+  db.sales.find({$and : [
+    {$or: [{purchaseMethod: "Online"},{"cuponUsed": true},{"customer" : {"age": {$lte: 25}}}]}
+  ]})
+
+asdfkalsdjfa = {
+    _id: ObjectId("5bd761dcae323e45a93cd049"),
+    saleDate: ISODate("2013-10-17T13:18:46.279Z"),
+    items: [
+      {
+        name: 'printer paper',
+        tags: [ 'office', 'stationary' ],
+        price: Decimal128("14.68"),
+        quantity: 5
+      },
+      {
+        name: 'backpack',
+        tags: [ 'school', 'travel', 'kids' ],
+        price: Decimal128("176.47"),
+        quantity: 3
+      },
+      {
+        name: 'envelopes',
+        tags: [ 'stationary', 'office', 'general' ],
+        price: Decimal128("24.88"),
+        quantity: 3
+      },
+      {
+        name: 'binder',
+        tags: [ 'school', 'general', 'organization' ],
+        price: Decimal128("13.41"),
+        quantity: 9
+      },
+      {
+        name: 'pens',
+        tags: [ 'writing', 'office', 'school', 'stationary' ],
+        price: Decimal128("33.55"),
+        quantity: 1
+      },
+      {
+        name: 'binder',
+        tags: [ 'school', 'general', 'organization' ],
+        price: Decimal128("20.21"),
+        quantity: 4
+      },
+      {
+        name: 'pens',
+        tags: [ 'writing', 'office', 'school', 'stationary' ],
+        price: Decimal128("70.71"),
+        quantity: 1
+      }
+    ],
+    storeLocation: 'San Diego',
+    customer: { gender: 'M', age: 49, email: 'aroru@ti.yt', satisfaction: 2 },
+    couponUsed: false,
+    purchaseMethod: 'In store'
+  }
+
+db.sales.find({
+  $and: [
+    {$or:[{"items.name": "pens"},{"items.tags": "writing"}]}
+  ]
+})
