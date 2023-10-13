1b711c25ed8c260e0260930c9083dbbadcfd409f
Marco Rossel <andresmarcorossel@gmail.com>
2023-10-13 17:12:31 -0300

    express course | style list

--- todoList/main.js
+++ todoList/main.js
@@ -1,6 +1,26 @@
-//const {updateTask} = require('./taskUpdate') --> esto es en node
 import updateTask from './taskUpdate.js'//--> asi es con javascript vanilla
 
-updateTask('testing modules')
+const enviar = document.querySelector('.enviar');
+const borrar = document.querySelector('.borrar');
+const editar = document.querySelector('.editar');
 
+enviar.addEventListener('click', function (ev) {
+    ev.preventDefault();
+    console.log('Presionase el boton enviar');
+})
+/* ojo aqui por que el boton enviar es uno, pero borrar y editar pueden ser muchos */
+borrar.addEventListener('click', function (ev) {
+    ev.preventDefault();
+    console.log('Presionase el boton borrar');
+})
+editar.addEventListener('click', function (ev) {
+    ev.preventDefault();
+    console.log('Presionase el boton editar');
+})
+
+updateTask('testing modules')
+/* (function () {
+"use strict";
+console.log('modo estricto activado');
 console.log("Hi !!!");
+})() */
