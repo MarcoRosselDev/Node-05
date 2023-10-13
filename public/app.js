7253e650d811a94bd451f320c88fc5e49e92a2ae
Marco Rossel <andresmarcorossel@gmail.com>
2023-10-13 09:57:48 -0300

    express course | CRUD mongoose | fetch vs axios 2

--- public/app.js
+++ public/app.js
@@ -5,35 +5,48 @@ const tasks = document.querySelector('.tasks');
 let lista = []
 
 button.addEventListener('click', function () {
-    lista.push(input.value)
-    let printDom = ''
-    for (let i = 0; i < lista.length; i++) {
-        printDom += `<li>${lista[i]}</li>`;
-    }
-    tasks.children[0].innerHTML = printDom;
-    input.value = '';
+  lista.push(input.value)
+  let printDom = ''
+  for (let i = 0; i < lista.length; i++) {
+    printDom += `<li>${lista[i]}</li>`;
+  }
+  tasks.children[0].innerHTML = printDom;
+  input.value = '';
 });
 
 const loadData = async () => {
-    try {
-        await fetch('/api/v1/tasks/tester', {
-            method: 'GET', headers: {
-                'Content-Type': 'application/json',
-                'Content-Type': 'application/x-www-form-urlencoded',
-            },
-        })
-        .then(data => data.json())
-        .then(a => console.log(a))
-            /* let elem = '';
-            // iterar por el arreglo y mostrarlo en la lista
-            for (let i = 0; i < a.length; i++) {
-                elem += `<li>${a[i]}</li>`
-            }
-            tasks.children[0].innerHTML = elem; */
-        .catch(error => console.error(error))
-    } catch (error) {
-        console.log(error);
+  try {
+    await fetch('/api/v1/tasks/tester', {
+      method: 'GET', headers: {
+        'Content-Type': 'application/json',
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+    })
+      .then(data => data.json())
+      .then(a => {
+        let elem = '';
+        // iterar por el arreglo y mostrarlo en la lista
+        for (let i = 0; i < a.length; i++) {
+          elem += `<li>${a[i].name} <button class="deleteButton">delete</button></li>`
+        }
+        tasks.children[0].innerHTML = elem;
+      })
+      .catch(error => console.error(error))
+
+    const deleteButtons = document.querySelectorAll('.deleteButton')
+    for (let i = 0; i < deleteButtons.length; i++) {
+      deleteButtons[i].addEventListener('click', function (eve) {
+        eve.preventDefault();
+        console.log('clicked delete button');
+      })
     }
+    /* deleteButton.addEventListener("click", function (event) {
+        event.preventDefault();
+        console.log("You're clicked delete button");
+    }) */
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 loadData();
