00f0893a1dc986f50b3af4e2a244560f9651ef14
Marco Rossel <andresmarcorossel@gmail.com>
2023-10-14 19:40:19 -0300

    express course | get data from mongodb successful

--- public/main.js
+++ public/main.js
@@ -18,6 +18,39 @@ console.log(newDiv);
 
 main.append(newDiv) */
 
+const cargarTareas = async () => {
+  try {
+    const response = await fetch("/api/v1/tasks/mongodriver", {
+      method: 'GET', headers: {
+        'Content-Type': 'application/json',
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+    })
+    .then(data => data.json())
+    .then((event) => {
+      console.log(event);
+      event.forEach((data) => {
+        const name = data.name
+        // crear un div y agregarlo al final
+        const newDiv = document.createElement("div");
+          newDiv.classList.add('lista')
+          newDiv.innerHTML = `
+          <p>${name}</p>
+          <div class="btns">
+            <button class="borrar">Borrar</button>
+            <button class="editar">Editar</button>
+          </div>`
+          main.append(newDiv)
+      }
+      )
+    })
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+cargarTareas()
+
 const tarea = async (data) => {
   try {
     const response = await fetch("/api/v1/tasks", {
@@ -28,31 +61,24 @@ const tarea = async (data) => {
       },
       body: JSON.stringify(data)
     })
-    .then((event) => {
-      console.log(event)
-      if (event.status == 201) {
-        console.log(event);
-        console.log('fue 201 !!!');
-        const newDiv = document.createElement("div");
-newDiv.classList.add('lista')
-newDiv.innerHTML = `<p>random text</p>
-<div class="btns">
-    <button class="borrar">Borrar</button>
-    <button class="editar">Editar</button>
-</div>`
-main.append(newDiv)
-        /* main.appendChild.innerHTML += ` ---> no sirver
-        <div class="lista">
-          <p>${input.value}</p>
+      .then((event) => {
+        console.log(event)
+        if (event.status == 201) {
+          console.log(event);
+          console.log('fue 201 !!!');
+          const newDiv = document.createElement("div");
+          newDiv.classList.add('lista')
+          newDiv.innerHTML = `
+          <p>random text</p>
           <div class="btns">
-              <button class="borrar">Borrar</button>
-              <button class="editar">Editar</button>
-          </div>
-        </div>` */
-      } else {
-        console.log(`status: ${event.status}`);
-      }
-    }) 
+            <button class="borrar">Borrar</button>
+            <button class="editar">Editar</button>
+          </div>`
+          main.append(newDiv)
+        } else {
+          console.log(`status: ${event.status}`);
+        }
+      })
   } catch (error) {
     console.error(error)
   }
@@ -62,9 +88,9 @@ enviar.addEventListener('click', function (ev) {
   ev.preventDefault();
   console.log('Presionase el boton enviar');
   console.log(input.value);
-  const body = {name: input.value}
+  const body = { name: input.value }
   tarea(body);
-  
+
 })
 /* ojo aqui por que el boton enviar es uno, pero borrar y editar pueden ser muchos */
 
