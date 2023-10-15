e06b8f9d2588323794b64b53ab398c096a460d24
Marco Rossel <andresmarcorossel@gmail.com>
2023-10-14 23:10:19 -0300

    express course | delete

--- public/borrarTarea.js
+++ public/borrarTarea.js
@@ -1,10 +1,10 @@
 //const borrar = document.querySelectorAll('.borrar');
 
-const borrarId = async (id) => {
+const borrarId = async (idDelete) => {
   try {
     const response = await fetch(`/api/v1/tasks/mongodriver`, {
       method: "DELETE",
-      body: {id: id}
+      body: {id: idDelete}
     })
     return response.json()
   } catch (error) {
@@ -13,14 +13,17 @@ const borrarId = async (id) => {
 }
 
 const borrarTarea = (btn) => {
-
   btn.forEach((borr) => {
     const id = borr.parentElement.parentElement.children[1].innerText;
     borr.addEventListener('click', function (ev) {
       ev.preventDefault();
       console.log('Presionase el boton borrar');
       console.log(`id: ${id} borrado`);
-      borrarId(id);
+      const f = borrarId(id);
+      console.log(f);
+      if (f) {
+        borr.parentElement.parentElement.classList.add('hiden');
+      }
     })
   })
 }
