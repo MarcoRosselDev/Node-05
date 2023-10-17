6cea8bcaee04232f47baa50a18a96d106f2fd430
Marco Rossel <andresmarcorossel@gmail.com>
2023-10-17 18:00:40 -0300

    express course | proj v2 4

--- v2/main.js
+++ v2/main.js
@@ -5,7 +5,17 @@ editarBtn.forEach(btn =>{
   btn.addEventListener('click', function (event) {
     event.preventDefault()
     console.log("actualizar btn");
+    console.log(this.parentElement.parentElement.children[2].classList);
+    let div = this.parentElement.parentElement.children[2].classList;
+    
+    if (div.contains('ocultar') == true) {
+      div.remove('ocultar')
+    } else{
+      div.add('ocultar')
+    }
   })
 })
+
+console.log('testing');
 
 //updateTask('testing modules')
