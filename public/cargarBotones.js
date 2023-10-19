e618f6c131a316e6f0386371c852e35aa52218de
Marco Rossel <andresmarcorossel@gmail.com>
2023-10-19 01:00:39 -0300

    express course | v2 update task

--- public/cargarBotones.js
+++ public/cargarBotones.js
@@ -1,15 +1,15 @@
 import borrarTask from './borrarTask.js';
-//import cargarDatos from './cargarDatos.js';
+import editarDato from './editarDato.js';
 const mainElement = document.querySelector('main');
 
 const cargarBotones = () => {
   //boton borrar
   const borrarBtn = document.querySelectorAll(".borrar");
   borrarBtn.forEach(btn => btn.addEventListener('click', function (event) {
+    event.preventDefault();
     const id = this.parentElement.parentElement.children[1].innerText;
     //const id = this.parentElement.parentElement;
     console.log(id);
-    event.preventDefault();
     const del = borrarTask(id);
     del .then(resp => resp.json())
       .then(()=>{
@@ -21,15 +21,32 @@ const cargarBotones = () => {
   //boton editar
   const editarBtn = document.querySelectorAll(".editar");
   editarBtn.forEach(btn => btn.addEventListener('click', function (event) {
+    event.preventDefault();
+    let name = this.parentElement.parentElement.children[0];
     const id = this.parentElement.parentElement.children[1].innerText;
-    const ocultar = this.parentElement.parentElement.children[3]
+    const editarInput = this.parentElement.parentElement.children[3].children[0];
+    const enviarEdicion = this.parentElement.parentElement.children[3].children[1];
+    const ocultar = this.parentElement.parentElement.children[3];
+    
+    enviarEdicion.addEventListener('click', function (event) {
+      event.preventDefault();
+      console.log('clickeaste enviar edicion');
+      let editValue = editarInput.value;
+      editarDato(id, editValue);
+      console.log(editValue);
+      name.innerText = editarInput.value;
+      editarInput.value = '';
+    })
+
+    console.log(id);
+    console.log(enviarEdicion);
+    console.log(editarInput);
     if (ocultar.classList.contains("ocultar") == true) {
       ocultar.classList.remove("ocultar")
     } else {
       ocultar.classList.add("ocultar")
     }
-    console.log(ocultar);
-    event.preventDefault();
+    console.log('boton editar');
     console.log('editar borrar');
   }))
 }
---
+++ public/editarDato.js
@@ -0,0 +1,14 @@
+const editarDato = async (id, textUpdate)=>{
+    try {
+        const bodyP = {"name": textUpdate}
+        const response = await fetch(`/api/v1/tasks/tester/${id}`, {
+            method: "PUT",
+            body: JSON.stringify(bodyP)
+        })
+        return response;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export default editarDato;
