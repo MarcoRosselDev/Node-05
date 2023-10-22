44d076f016910b463f21237a254b500aec805181
Marco Rossel <andresmarcorossel@gmail.com>
2023-10-22 04:51:19 -0300

    api v2 | find and filter solved

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const connectionDB = require('./db/db.js')
 const notFoundMiddleware = require('./middleware/not-found.js');
 const errorMiddleware = require('./middleware/error-handler.js');
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false })); 
 //middleware
 app.use(express.json())
 
@@ -17,17 +17,11 @@ app.use('/api/v1', router);
 
 app.use(notFoundMiddleware);
 app.use(errorMiddleware);
-app.get('/', (req, res) => {
-  res.status(200).send("<h1>Hola mundo !</h1>")
-})
 
 const run = async () => {
   try {
     await connectionDB(process.env.MONGO_URL);
-    //conectar con mongoDB
-    app.listen(puerto, () => {
-      console.log(`Escuchando en el puerto ${puerto}`);
-    })
+    app.listen(puerto, () => console.log(`Escuchando en el puerto ${puerto}`))
   } catch (error) {
     console.error(error);
   }
--- controllers/mongoose.js
+++ controllers/mongoose.js
@@ -4,8 +4,9 @@ const Product = require('../schemas/models.js');
 // http:.../mdb?company=ikea ---> ahora esta en req.params para ser usado en find
 const todosLosDocumentos = async (req, res) => {
   try {
-    console.log(req.params);
-    const respuesta = await Product.find(req.params);
+    console.log(req.query);
+    //es query no params !!!
+    const respuesta = await Product.find(req.query);
     res.status(200).json(respuesta);
   } catch (error) {
     res.status(500).json(error);
