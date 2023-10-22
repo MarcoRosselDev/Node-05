920b586a6f4fa46164607bdd650e942cf3e0d663
Marco Rossel <andresmarcorossel@gmail.com>
2023-10-22 04:33:11 -0300

    api v2 | find and filter 2

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
-const express = require('express');
 require('express-async-errors');// puedo usar manejador de errores asyncrono|
+const express = require('express');
 const app = express();
 const puerto = 5000 || process.env.PORT;
 const router = require('./router/router.js');
@@ -9,25 +9,28 @@ const connectionDB = require('./db/db.js')
 const notFoundMiddleware = require('./middleware/not-found.js');
 const errorMiddleware = require('./middleware/error-handler.js');
 
+app.use(express.urlencoded({ extended: false }));
 //middleware
 app.use(express.json())
 
 app.use('/api/v1', router);
 
-app.get('/', (req, res)=>{
-    res.status(200).send("<h1>Hola mundo !</h1>")
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+app.get('/', (req, res) => {
+  res.status(200).send("<h1>Hola mundo !</h1>")
 })
 
 const run = async () => {
-    try {
-        await connectionDB(process.env.MONGO_URL);
-        //conectar con mongoDB
-        app.listen(puerto, () => {
-            console.log(`Escuchando en el puerto ${puerto}`);
-        })
-    } catch (error) {
-        console.error(error);
-    }
+  try {
+    await connectionDB(process.env.MONGO_URL);
+    //conectar con mongoDB
+    app.listen(puerto, () => {
+      console.log(`Escuchando en el puerto ${puerto}`);
+    })
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 run()
--- controllers/mongoose.js
+++ controllers/mongoose.js
@@ -1,11 +1,11 @@
 const Product = require('../schemas/models.js');
 
 // lista de companys para buscar = ['ikea', 'liddy', 'caressa', 'marcos']
+// http:.../mdb?company=ikea ---> ahora esta en req.params para ser usado en find
 const todosLosDocumentos = async (req, res) => {
   try {
-    const respuesta = await Product.find({
-      company: 'liddy'
-    });
+    console.log(req.params);
+    const respuesta = await Product.find(req.params);
     res.status(200).json(respuesta);
   } catch (error) {
     res.status(500).json(error);
