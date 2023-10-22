e6b5f04a9e96cdee3fdf19ee2334fb0f27c669f0
Marco Rossel <andresmarcorossel@gmail.com>
2023-10-22 07:27:14 -0300

    api v2 | evitando parametros no deseados

--- controllers/mongoose.js
+++ controllers/mongoose.js
@@ -4,9 +4,19 @@ const Product = require('../schemas/models.js');
 // http:.../mdb?company=ikea ---> ahora esta en req.params para ser usado en find
 const todosLosDocumentos = async (req, res) => {
   try {
+    // para evitar querys no permitidas desestrucuramos la query
+    const {company} = req.query;
+    const queryObject = {};
     console.log(req.query);
-    //es query no params !!!
-    const respuesta = await Product.find(req.query);
+
+    if (company) {
+      queryObject.company = req.query.company;
+    }
+    console.log(queryObject);
+    // asi nos evitamos querys no deseadas
+    // exapmle: /mdb?company=marcos&page=2
+    // no considera la req.query.page----> es ignorado
+    const respuesta = await Product.find(queryObject);
     res.status(200).json(respuesta);
   } catch (error) {
     res.status(500).json(error);
