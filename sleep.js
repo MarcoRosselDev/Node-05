9c6fe4afbd257b61b4ad7cceaa02cefc78752491
Marco Rossel <andresmarcorossel@gmail.com>
2023-10-23 08:46:15 -0300

    api v2 | sleep leetCode 01

---
+++ sleep.js
@@ -0,0 +1,32 @@
+/*Example 1:
+
+Input: millis = 100
+Output: 100
+Explanation: It should return a promise that resolves after 100ms.
+let t = Date.now();
+sleep(100).then(() => {
+  console.log(Date.now() - t); // 100
+});
+Example 2:
+
+Input: millis = 200
+Output: 200
+Explanation: It should return a promise that resolves after 200ms.
+ 
+
+Constraints:
+
+1 <= millis <= 1000 */
+
+async function sleep(millis) {
+  try {
+    await setTimeout(() => {
+      return millis
+    }, millis)
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+let t = Date.now()
+sleep(100).then(() => console.log(Date.now() - t)) // 100
