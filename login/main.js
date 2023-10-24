11e256961e30ab4ecac597ec136c091049f439d8
Marco Rossel <andresmarcorossel@gmail.com>
2023-10-24 04:35:52 -0300

    api v2 | login frontend 3

--- login/main.js
+++ login/main.js
@@ -1,1 +1,6 @@
-console.log('main js');
+const btnDark = document.querySelector('.btnDark');
+
+btnDark.addEventListener('click', function (event) {
+    event.preventDefault();
+    console.log('click en el boton dark mode');
+})
