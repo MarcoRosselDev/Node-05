d17b655daaf7e5fad276c4fe08841089c1773531
Marco Rossel <andresmarcorossel@gmail.com>
2023-10-24 06:19:23 -0300

    api v2 | login frontend 4

--- login/main.js
+++ login/main.js
@@ -1,6 +1,15 @@
 const btnDark = document.querySelector('.btnDark');
+const body = document.querySelector('body');
+const header = document.querySelector('header');
 
 btnDark.addEventListener('click', function (event) {
     event.preventDefault();
     console.log('click en el boton dark mode');
+    console.log(body.classList);
+    if (body.classList.contains('ligth') === true) {
+        body.classList.remove('ligth')
+    } else{
+        body.classList.add('ligth')
+    }
+    console.log(header.classList);
 })
