f09f3211efaf1bf8f9d89fd28bc647effa76df83
Marco Rossel <andresmarcorossel@gmail.com>
2023-10-25 10:41:44 -0300

    api v2 | jwtoken 2

--- jwt/jwt.js
+++ jwt/jwt.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 
+require('dotenv').config();
+const SECRET = process.env.SECRET;
+
 const app = express();
 
 app.post('/token', (req, res) => {
-  const token = null
+  // usuario se deve obtener de la db
+  // conectar con mongodb y match estos valores
+  // const email = db.find();
+  const nombre = 'marco';
+  const {id:sub} = '123asdfa1'; // renombramiento
+  //----------------------------------
+  const token = jwt.sign({
+    sub,
+    nombre,
+    exp: Date.now() + 60 * 1000 *5 // = 1 minuto (trabajamos con milisegundos)
+  }, SECRET)
   res.status(201).json({ 'token': token })
 })
 
@@ -14,9 +27,19 @@ app.get('/public', (req, res) => {
 
 app.get('/privado', (req, res) => {
   try {
+    // Bearer asldkfjalskdjf123...
+    // para sacar ese espacio y querarnos solo con el token text ===
+    const token = req.headers.authorization.split(' ')[1];
+    // para accesder al payload del token usamos la utilidad de jwt.verify()
+    const payload = jwt.verify(token, SECRET)
+
+    if (Date.now() > payload.exp) {
+      return res.status(401).send({error: 'token expirado!'})
+    }
+    // else res data privada
     res.status(200).json({'data': 'data privada'})
   } catch (error) {
-    res.status(401).json({'error': error})
+    res.status(401).json({err: error})
   }
 })
 
