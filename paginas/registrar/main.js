c8deb65f47751cdbfd01fef56dd716a4baea914b
Marco Rossel <andresmarcorossel@gmail.com>
2023-10-25 22:39:32 -0300

    api v2 | registrar nuevo usuario 3

---
+++ paginas/registrar/main.js
@@ -0,0 +1,22 @@
+const btnDark = document.querySelector('.btnDark');
+const body = document.querySelector('body');
+const header = document.querySelector('header');
+const loginBtn = document.querySelector('.loginBtn');
+const proyectos = document.querySelector('.proyectos');
+
+btnDark.addEventListener('click', function (event) {
+  event.preventDefault();
+  if (body.classList.contains('ligth') === true) {
+    body.classList.remove('ligth')
+    btnDark.textContent = 'Light mode';
+    header.classList.remove('ligth-header')
+  } else {
+    body.classList.add('ligth')
+    btnDark.textContent = 'Darck mode';
+    header.classList.add('ligth-header')
+  }
+})
+
+loginBtn.addEventListener('click', () => {
+  console.log('login bnt');
+})
