a0f310b583ebef53f05b66f2e8d5b7fecb6bd2be
Marco Rossel <andresmarcorossel@gmail.com>
2023-10-25 23:59:09 -0300

    api v2 | responsive style first

--- paginas/registrar/main.js
+++ paginas/registrar/main.js
@@ -4,6 +4,14 @@ const header = document.querySelector('header');
 const loginBtn = document.querySelector('.loginBtn');
 const proyectos = document.querySelector('.proyectos');
 
+const menuBtn = document.querySelector('.burgerMenu');
+
+menuBtn.addEventListener('click', (e)=>{
+  e.preventDefault();
+  console.log('menu btn'); 
+  console.log('mostrar lista desplegable'); 
+})
+
 btnDark.addEventListener('click', function (event) {
   event.preventDefault();
   if (body.classList.contains('ligth') === true) {
